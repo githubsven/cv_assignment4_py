646d541cc46d859d5aadf827eac2119d67f858e1
Sven Luehof <s.luehof@gmail.com>
2018-03-21 16:00:23 +0100

    Auto stash before merge of "master" and "origin/master"

--- imgUtils.py
+++ imgUtils.py
@@ -5,11 +5,44 @@ Created on Wed Mar 21 15:20:47 2018
 
 Contains utilities to work with images
 
-@author: breixo
+@author: Breixo, Sven
 """
 
 import numpy as np
 import cv2
+import os
+
+def createData():
+    """
+    Creates images to feed to the Convolutional Neural Network
+    It takes the frame at the middle of the video, and converts this to a png
+    """
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    dataFolder = dir_path + "/data/ucf-101/"
+    outputFolder = dir_path + "/data/training/"
+    if not os.path.exists(outputFolder):
+        os.makedirs(outputFolder)
+    dataTypes = ["BrushingTeeth", "CuttingInKitchen", "JumpingJack", "Lunges", "WallPushups"]
+
+    for type in dataTypes:
+        if not os.path.exists(outputFolder + type):
+            os.makedirs(outputFolder + type)
+
+        videos = []
+        for (dirpath, dirnames, filenames) in os.walk(dataFolder + type + "/"):
+            videos.extend(filenames)
+            break # Only needs to be executed once, because filenames is an array
+
+        for video in videos:
+            cap = cv2.VideoCapture(dataFolder + type + "/" + video)
+            halfpoint_frame = cap.get(7) // 2
+            cap.set(1, halfpoint_frame)
+            ret, frame = cap.read()
+            croppedFrame = imageToSquare(frame)
+            fileName = str.split(video, ".")[0]
+            cv2.imwrite(outputFolder + type + "/" + fileName + ".png", croppedFrame)
+            return
+
 
 def imageToSquare(img):
     """
@@ -30,6 +63,7 @@ def resizeImage(img, newSize = (90, 90)):
     OpenCV function wrapper
     """
     return cv2.resize(img, newSize)
+
 
 def showImage(img, name = "Image"):
     """
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 import tensorflow as tf
-import os
+import imgUtils
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
@@ -26,38 +26,12 @@ def test():
     pass # Does nothing
 
 
-def main():
+def main(argv):
     processVideos()
     train()
     test()
 
 
-def createData():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    dataFolder = dir_path + "/data/ucf-101/"
-    outputFolder = dir_path + "/data/training/"
-    if not os.path.exists(outputFolder):
-        os.makedirs(outputFolder)
-    dataTypes = ["BrushingTeeth", "CuttingInKitchen", "JumpingJack", "Lunges", "WallPushups"]
-
-    for type in dataTypes:
-        if not os.path.exists(outputFolder + type):
-            os.makedirs(outputFolder + type)
-        videos = []
-        for (dirpath, dirnames, filenames) in os.walk(dataFolder + type + "/"):
-            videos.extend(filenames)
-            break
-
-        for video in videos:
-            cap = cv2.VideoCapture(dataFolder + type + "/" + video)
-            halfpoint_frame = round(cap.get(7) / 2)
-            cap.set(1, halfpoint_frame)
-            ret, frame = cap.read()
-            fileName = str.split(video, ".")[0]
-            cv2.imwrite(outputFolder + type + "/" + fileName + ".png", frame)
-            break
-
-
 if __name__ == "__main__":
-    createData()
+    imgUtils.createData()
     #tf.app.run()
