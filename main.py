fd577dc76832517cd7353b97c7ac1a47b97ade0a
breixo <breixo.solino@gmail.com>
2018-03-21 14:32:09 +0100

    Added base functions to code

--- main.py
+++ main.py
@@ -3,5 +3,24 @@ import tensorflow as tf
 
 #Edit paper: https://www.sharelatex.com/5152532135sdnhgphhqsbd
 
+def processVideos():
+    #TODO Read videos and transform them to input data
+    pass # Does nothing
+
+def train():
+    #TODO Train the NN
+    pass # Does nothing
+
+def test():
+    #TODO Test the NN
+    pass # Does nothing
+
+
+def main():
+    processVideos()
+    train()
+    test()
+
+
 if __name__ == "__main__":
-    print("x")
+    main()
